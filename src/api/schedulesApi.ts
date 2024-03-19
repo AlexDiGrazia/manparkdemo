ebfdeb3d4fc5c5a7b5d82e808c86302ccb3f1dc6
Alex Digrazia <alexndigrazia@hotmail.com>
2024-03-18 21:36:45 -0700

    rewrote getCurrentUserScheduleData() to query database based off of user's name, rather than getting all schedule data and filtering on the frontend

--- src/api/schedulesApi.ts
+++ src/api/schedulesApi.ts
@@ -4,9 +4,7 @@ const BASE_URL = "http://localhost:3000";
 
 export const Requests = {
   getCurrentUserScheduleData: (user: string) =>
-    fetch(`${BASE_URL}/schedules`)
-      .then((res) => res.json())
-      .then((res) => res.filter((obj: TSchedules) => obj.user === user)),
+    fetch(`${BASE_URL}/schedules/${user}`).then((res) => res.json()),
 
   postNewScheduleData: (newData: Omit<TSchedules, "id">) =>
     fetch(`${BASE_URL}/schedules`, {
