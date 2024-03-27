3c977c1ffc8a1ba9097518106c3de267efd187cd
Alex Digrazia <alexndigrazia@hotmail.com>
2024-03-26 18:31:16 -0700

    added jwtToken argument and authorization header to postNewScheduleData(), deleteManyScheduleAppointments(), and updateAppointment(). Also omitted the 'user' from newData in postNewScheduleData() since the user is now being supplied by the authMiddleware

--- src/api/schedulesApi.ts
+++ src/api/schedulesApi.ts
@@ -6,34 +6,44 @@ export const Requests = {
   getCurrentUserScheduleData: (user: string) =>
     fetch(`${BASE_URL}/schedules/${user}`).then((res) => res.json()),
 
-  postNewScheduleData: (newData: Omit<TSchedules, "id">) =>
+  postNewScheduleData: (
+    newData: Omit<TSchedules, "id" | "user">,
+    jwtToken: string
+  ) =>
     fetch(`${BASE_URL}/schedules`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        Authorization: "Bearer " + jwtToken,
       },
       body: JSON.stringify(newData),
     }).then((res) => res.json()),
 
-  deleteManyScheduleAppointments: (deletionQueue: {
-    deletionQueue: number[];
-  }) =>
+  deleteManyScheduleAppointments: (
+    deletionQueue: {
+      deletionQueue: number[];
+    },
+    jwtToken: string
+  ) =>
     fetch(`${BASE_URL}/schedules/delete`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        Authorization: "Bearer " + jwtToken,
       },
       body: JSON.stringify(deletionQueue),
     }).then((res) => res.json()),
 
   updateScheduleAppointment: (
     id: number,
-    updateAppointment: { event: string }
+    updateAppointment: { event: string },
+    jwtToken: string
   ) =>
     fetch(`${BASE_URL}/schedules/${id}`, {
       method: "PATCH",
       headers: {
         "Content-Type": "application/json",
+        Authorization: "Bearer " + jwtToken,
       },
       body: JSON.stringify(updateAppointment),
     }),
