a6e5bc443de9fe0dff539c07e5721fdea976f916
Alex Digrazia <alexndigrazia@hotmail.com>
2024-03-26 17:33:35 -0700

    added authorization header and jwtToken argument to updateProfile()

--- src/api/profilesApi.ts
+++ src/api/profilesApi.ts
@@ -8,11 +8,12 @@ export const Requests = {
   getSingleProfile: (id: number) =>
     fetch(`${BASE_URL}/profiles/${id}`).then((res) => res.json()),
 
-  updateProfile: (id: number, profile: Partial<TProfile>) =>
+  updateProfile: (id: number, profile: Partial<TProfile>, jwtToken: string) =>
     fetch(`${BASE_URL}/profiles/${id}`, {
       method: "PATCH",
       headers: {
         "Content-Type": "application/json",
+        Authorization: "Bearer " + jwtToken,
       },
       body: JSON.stringify(profile),
     }).then((res) => res.json()),
