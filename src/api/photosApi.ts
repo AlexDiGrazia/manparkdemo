b7a058c22824a34739ad74294c7c17e18514c248
Alex Digrazia <alexndigrazia@hotmail.com>
2024-03-28 23:42:31 -0700

    created postNewPhoto() fetch call

--- src/api/photosApi.ts
+++ src/api/photosApi.ts
@@ -2,4 +2,13 @@ const BASE_URL = "http://localhost:3000";
 
 export const Requests = {
   getAllPhotos: () => fetch(`${BASE_URL}/photos`).then((res) => res.json()),
+
+  postNewPhoto: (newPhoto: { image: string; date: string }) =>
+    fetch(`${BASE_URL}/photos/upload`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPhoto),
+    }),
 };
